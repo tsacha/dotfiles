4d12d5fa151edde770e3fa3a7b190c070f42efe4
Sacha Trémoureux <sacha@findpsire.com>
2013-09-23 16:01:28 +0200

    i3 configuration

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env sh
 
-# Emacs deployment
+# Emacs
 cp emacs/emacs.el ~/.emacs.el
 mkdir -p ~/.config/emacs
 mkdir -p ~/.emacs.d/{auto-save-list,cache,elpa,url}
 cp -Rf emacs/config/* ~/.config/emacs
 
-# Shells deployment
+# Shells
 cp bashrc ~/.bashrc
 cp zshrc ~/.zshrc
 cp zshrc.pre ~/.zshrc.pre
 cp zshrc.local ~/.zshrc.local
+
+# i3
+cp -Rf i3/ ~/.i3
+cp i3/i3status.conf ~/.i3status.conf
