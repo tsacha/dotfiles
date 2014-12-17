7dbdcf98b3c14121a05661c765107e84bda287ef
Sacha Trémoureux <sacha@tremoureux.fr>
2014-12-17 10:58:20 +0100

    Emacs configuration is now on a different repository

--- emacs/config/init-30-utils.el
+++
@@ -1,8 +0,0 @@
-;; undo-tree
-(require 'undo-tree)
-;; Lighter displayed in mode line
-(setq undo-tree-mode-lighter " UT")
-;; ...and enable!
-(global-undo-tree-mode)
-
-
--- emacs/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-[ ! -h ~/.emacs.el ] && ln -s `pwd`/emacs.el ~/.emacs.el
-[ ! -d ~/.config/emacs ] && mkdir -p ~/.config/emacs
-find `pwd`/config -type f -exec ln -s {} ~/.config/emacs/ 2> /dev/null \;
-
