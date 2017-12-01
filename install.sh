a810e97d9faa553ee8960ff3abc09d1395668e3e
Sacha Trémoureux <sacha@localhost.localdomain>
2017-12-01 14:05:47 +0100

    December fix

--- install.sh
+++ install.sh
@@ -236,15 +236,15 @@ arch-chroot /mnt pacman -Sy --noconfirm yaourt
 read -r -p "Install desktop environment y/N? : " desktop
 echo
 if [ "$desktop" == "y" ]; then
-    arch-chroot /mnt pacman -S --noconfirm xorg-server mesa xf86-input-libinput xf86-input-synaptics xf86-video-intel xorg-xbacklight xorg-xinit emacs auctex i3-wm i3lock i3status rofi dmenu conky st xfce4-terminal thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman pulseaudio pavucontrol compton ttf-dejavu adobe-source-code-pro-fonts gajim feh firefox thunderbird libreoffice-fresh sxiv redshift okular vinagre freerdp spice phonon-qt4-gstreamer transmission-qt qt4 xfce4-notifyd vlc evince atom texlive-most texlive-lang inkscape pandoc ttf-liberation ttf-dejavu ttf-linux-libertine ttf-linux-libertine-g arandr sway network-manager-applet sddm keybase ttf-fira-sans ttf-fira-mono pass ipcalc virt-manager openssh-askpass virt-viewer qemu qemu-arch-extra qemu-guest-agent  samba cups a2ps wireshark-gtk vnstat scrot gimp markdown gnome-alsamixer alsa-utils pamixer
+    arch-chroot /mnt pacman -S --noconfirm xorg-server mesa xf86-input-libinput xf86-input-synaptics xf86-video-intel xorg-xbacklight xorg-xinit emacs auctex i3-wm i3lock i3status rofi dmenu conky xfce4-terminal thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman pulseaudio pavucontrol compton ttf-dejavu adobe-source-code-pro-fonts gajim feh firefox thunderbird libreoffice-fresh sxiv redshift okular vinagre freerdp spice phonon-qt4-gstreamer transmission-qt qt4 xfce4-notifyd vlc evince atom texlive-most inkscape pandoc ttf-liberation ttf-dejavu ttf-linux-libertine ttf-linux-libertine-g arandr sway network-manager-applet sddm keybase ttf-fira-sans ttf-fira-mono pass ipcalc virt-manager openssh-askpass virt-viewer qemu qemu-arch-extra qemu-guest-agent  samba cups a2ps wireshark-gtk vnstat scrot gimp markdown gnome-alsamixer alsa-utils pamixer
 
-    arch-chroot systemctl enable org.cups.cupsd
-    arch-chroot systemctl enable cups-browsed.service
-    arch-chroot systemctl enable samba.service
-    arch-chroot systemctl enable nscd.service
-    usermod -G lp -a sacha
-    usermod -G libvirt -a sacha
-    usermod -G kvm -a sacha
+    arch-chroot /mnt systemctl enable org.cups.cupsd
+    arch-chroot /mnt systemctl enable cups-browsed.service
+    arch-chroot /mnt systemctl enable samba.service
+    arch-chroot /mnt systemctl enable nscd.service
+    arch-chroot /mnt usermod -G lp -a sacha
+    arch-chroot /mnt usermod -G libvirt -a sacha
+    arch-chroot /mnt usermod -G kvm -a sacha
     arch-chroot /mnt systemctl enable sddm
 
     if [ "$vmware" == "y" ]; then
