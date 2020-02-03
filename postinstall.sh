4f74d8200a88918fb3086de8a7b3f191306b614f
Sacha Trémoureux <sacha@tremoureux.fr>
2020-02-03 22:01:38 +0100

    desktop 2020

--- postinstall.sh
+++ postinstall.sh
@@ -1,45 +1,4 @@
 #!/bin/bash
-# Aurman installation part : 
-# darim92 - https://github.com/darim92/aurman-install/blob/master/aurman_install.sh
-# make tmp dir
-mkdir -p aurman_install
-cd aurman_install
-
-echo "installing dependencies..."
-
-# install dependencies
-sudo pacman -Sy binutils make gcc fakeroot pkg-config --noconfirm --needed
-sudo pacman -S git python python-regex python-requests pyalpm --noconfirm --needed
-
-echo "installing expac-git..."
-
-# install expac-git
-curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=expac-git
-makepkg PKGBUILD --skippgpcheck --install --noconfirm --needed
-
-echo "installing aurman..."
-
-# install aurman
-curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=aurman
-makepkg PKGBUILD --skippgpcheck --install --noconfirm --needed
-
-# clean up
-cd ..
-rm -rf aurman_install
-echo "-------------"
-echo "enjoy aurman!"
-echo "-------------"
-
-# install pamac
-read -p "install pamac? [y/n]" answer
-if [[ $answer = y ]] ; then
-  aurman -S pamac-aur --noconfirm
-  echo "pamac is ready..."
-  echo "-------------"
-fi
-
-echo "end of script..."
-
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub com.spotify.Client
 sudo ln -s /var/lib/flatpak/exports/bin/com.spotify.Client /usr/local/bin/spotify
@@ -48,4 +7,9 @@ sudo ln -s /var/lib/flatpak/exports/bin/org.telegram.desktop /usr/local/bin/tele
 flatpak install -y flathub com.visualstudio.code
 sudo ln -s /var/lib/flatpak/exports/bin/com.visualstudio.code /usr/local/bin/vscode
 
-aurman -S --noconfirm --noedit rofi-pass
+cd $HOME
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay -S --noconfirm --noedit rofi-pass
