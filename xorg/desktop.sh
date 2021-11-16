99b105f7f522525b15808b3cef619558630547ae
Sacha Trémoureux <sacha@tremoureux.fr>
2021-11-16 14:38:05 +0100

    rates

--- xorg/desktop.sh
+++ xorg/desktop.sh
@@ -2,11 +2,11 @@
 if [ ! -d /sys/class/power_supply/BAT0 ]; then
     xrandr \
         --output HDMI-0 --off \
-        --output DP-0 --primary --mode 3440x1440 --pos 5280x560 --rotate normal \
+        --output DP-0 --primary --mode 3440x1440 --pos 5280x560 --rate 143.92 --rotate normal \
         --output DP-1 --off \
         --output DP-2 --mode 1920x1080 --pos 8720x920 --rotate normal \
         --output DP-3 --off \
-        --output DP-4 --mode 2560x1440 --pos 3840x0 --rotate left \
+        --output DP-4 --mode 2560x1440 --pos 3840x0 --rate 144.00 --rotate left \
         --output DP-5 --off
 
     feh --bg-fill /home/sacha/Pictures/wallpapers/gris/01.png --bg-fill /home/sacha/Pictures/wallpapers/gris/02.png --bg-fill /home/sacha/Pictures/wallpapers/gris/03.png
--- xorg/tv.sh
+++ xorg/tv.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 xrandr \
     --output HDMI-0 --mode 3840x2160 --pos 0x200 --rotate normal \
-    --output DP-0 --primary --mode 3440x1440 --pos 5280x560 --rotate normal \
+    --output DP-0 --primary --mode 3440x1440 --pos 5280x560 --rate 143.92 --rotate normal \
     --output DP-1 --off \
     --output DP-2 --mode 1920x1080 --pos 8720x920 --rotate normal \
     --output DP-3 --off \
-    --output DP-4 --mode 2560x1440 --pos 3840x0 --rotate left \
+    --output DP-4 --mode 2560x1440 --pos 3840x0 --rate 144.0 --rotate left \
     --output DP-5 --off
 
 feh --bg-fill /home/sacha/Pictures/wallpapers/gris/01.png --bg-fill /home/sacha/Pictures/wallpapers/gris/02.png --bg-fill /home/sacha/Pictures/wallpapers/gris/03.png --bg-fill /home/sacha/Pictures/wallpapers/gris/04.png
