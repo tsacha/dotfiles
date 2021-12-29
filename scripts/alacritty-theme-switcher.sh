8fc5ceb916af8feb798c3e26b94212b6c51b1119
Sacha Trémoureux <sacha@tremoureux.fr>
2021-12-29 18:36:30 +0100

    Theme switcher support

--- scripts/alacritty-theme-switcher.sh
+++ scripts/alacritty-theme-switcher.sh
@@ -2,7 +2,9 @@
 if [ $(gsettings get org.gnome.desktop.interface gtk-theme) == "'Adwaita'" ]; then
     gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
     gsettings set org.gnome.desktop.interface icon-theme 'Adwaita-dark'
+    chezmoi apply --force ~/.config/alacritty/alacritty.yml ~/.config/rofi/config.rasi
 elif [ $(gsettings get org.gnome.desktop.interface gtk-theme) == "'Adwaita-dark'" ]; then
     gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
     gsettings set org.gnome.desktop.interface icon-theme 'Adwaita'
+    chezmoi apply --force ~/.config/alacritty/alacritty.yml ~/.config/rofi/config.rasi
 fi;
