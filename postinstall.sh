ee4eb98b97a104c7e611bd924caad8ac9b29e7cd
Sacha Trémoureux <sacha@tremoureux.fr>
2021-12-29 18:54:51 +0100

    Import previous postinstall script

---
+++ postinstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub com.spotify.Client
+flatpak install -y flathub com.visualstudio.code
+sudo ln -s /var/lib/flatpak/exports/bin/com.visualstudio.code /usr/local/bin/vscode
+
+cd $HOME
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay -S rofi-pass i3-gnome onedriver
