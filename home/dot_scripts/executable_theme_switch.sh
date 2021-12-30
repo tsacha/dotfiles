0994abcf1e3574b401745c52b23bbc000e60e1a3
Sacha Trémoureux <sacha@tremoureux.fr>
2021-12-30 15:28:51 +0100

    Chezmoi custom directory in scripts

--- home/dot_scripts/executable_theme_switch.sh
+++ home/dot_scripts/executable_theme_switch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+cm="chezmoi -S ~/Git/dotfiles -c ~/Git/dotfiles/chezmoi.toml --persistent-state ~/.config/chezmoi/chezmoistate.boltdb"
 if [ $(gsettings get org.gnome.desktop.interface gtk-theme) == "'Adwaita'" ]; then
     gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
     gsettings set org.gnome.desktop.interface icon-theme 'Adwaita-dark'
-    chezmoi apply --force ~/.config/alacritty/alacritty.yml ~/.config/rofi/config.rasi
+    $cm apply --force ~/.config/alacritty/alacritty.yml ~/.config/rofi/config.rasi
 elif [ $(gsettings get org.gnome.desktop.interface gtk-theme) == "'Adwaita-dark'" ]; then
     gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
     gsettings set org.gnome.desktop.interface icon-theme 'Adwaita'
-    chezmoi apply --force ~/.config/alacritty/alacritty.yml ~/.config/rofi/config.rasi
+    $cm apply --force ~/.config/alacritty/alacritty.yml ~/.config/rofi/config.rasi
 fi;
