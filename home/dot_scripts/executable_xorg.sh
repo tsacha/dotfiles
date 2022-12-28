428652769ffca5bc6805bcdc8a726cebae9a6555
Sacha Trémoureux <sacha@tremoureux.fr>
2022-12-28 19:07:46 +0100

    MOAR DISPLAYS

--- home/dot_scripts/executable_xorg.sh
+++ home/dot_scripts/executable_xorg.sh
@@ -2,11 +2,11 @@
 if [ ! -d /sys/class/power_supply/BAT0 ]; then
     xrandr \
         --output HDMI-0 --off \
-        --output DP-0 --mode 3440x1440 --pos 0x0 --rate 143.92 --rotate normal \
+        --output DP-0 --mode 3440x1440 --pos 0x720 --rate 143.92 --rotate normal \
         --output DP-1 --off \
-        --output DP-2 --mode 1920x1080 --pos 3440x0 --rate 143.92 --rotate normal \
+        --output DP-2 --mode 1920x1080 --pos 3440x1080 --rate 143.85 --rotate normal \
         --output DP-3 --off \
-        --output DP-4 --off \
+        --output DP-4 --mode 1920x1080 --pos 3440x0 --rate 143.98 --rotate normal \
         --output DP-5 --off
     nvidia-settings --load-config-only
 fi
