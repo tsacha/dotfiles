5c7fdf7de9750887a3a4788c83fe8303cb8eaaea
Sacha Trémoureux <sacha@tremoureux.fr>
2023-03-24 18:41:58 +0100

    k9s skin

--- home/dot_scripts/executable_theme_switch.sh
+++ home/dot_scripts/executable_theme_switch.sh
@@ -3,9 +3,9 @@ cm="chezmoi -S ~/Git/dotfiles -c ~/Git/dotfiles/chezmoi.toml --persistent-state 
 if [ $(gsettings get org.gnome.desktop.interface gtk-theme) == "'Adwaita'" ]; then
     gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
     gsettings set org.gnome.desktop.interface icon-theme 'Adwaita-dark'
-    $cm apply --force ~/.config/alacritty/alacritty.yml ~/.config/rofi/config.rasi
+    $cm apply --force ~/.config/alacritty/alacritty.yml ~/.config/rofi/config.rasi ~/.config/k9s/skin.yml
 elif [ $(gsettings get org.gnome.desktop.interface gtk-theme) == "'Adwaita-dark'" ]; then
     gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
     gsettings set org.gnome.desktop.interface icon-theme 'Adwaita'
-    $cm apply --force ~/.config/alacritty/alacritty.yml ~/.config/rofi/config.rasi
+    $cm apply --force ~/.config/alacritty/alacritty.yml ~/.config/rofi/config.rasi ~/.config/k9s/skin.yml
 fi;
