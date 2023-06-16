015a3a7350dbd37125ce9605b70b68be5fd33527
Sacha Trémoureux <sacha@tremoureux.fr>
2023-06-16 11:04:27 +0200

    Encore des changements d’écrans

--- home/dot_scripts/executable_xorg.sh
+++ home/dot_scripts/executable_xorg.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ "$(sudo virt-what)" != "hyperv" ]; then
     xrandr \
-        --output HDMI-0 --off \
-        --output DP-0 --mode 1920x1080 --pos 3440x0 --rate 143.98 --rotate normal \
+        --output DP-0 --mode 3440x1440 --pos 200x1080   --rate 143.92 --rotate normal --primary \
+        --output DP-4 --mode 1920x1080 --pos 0x0        --rate 143.98 --rotate normal \
+        --output DP-2 --mode 1920x1080 --pos 1920x0     --rate 143.85 --rotate normal \
         --output DP-1 --off \
-        --output DP-2 --mode 1920x1080 --pos 3440x1080 --rate 143.85 --rotate normal \
+        --output HDMI-0 --off \
         --output DP-3 --off \
-        --output DP-4 --mode 3440x1440 --pos 0x720 --rate 143.92 --rotate normal \
         --output DP-5 --off
     nvidia-settings --load-config-only
     systemctl --user start picom
