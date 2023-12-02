4cb2fa16bcee51db55540c75ab3d97c7034143cb
Sacha Trémoureux <sacha@tremoureux.fr>
2023-12-02 01:01:51 +0100

    D’autres trucs

--- home/dot_scripts/executable_xorg.sh
+++ home/dot_scripts/executable_xorg.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 if [ "$(sudo virt-what)" != "hyperv" ]; then
     xrandr \
-        --output DP-0 --mode 3440x1440 --pos 200x1080   --rate 143.92 --rotate normal --primary \
+        --output DP-2 --mode 3440x1440 --pos 200x1080   --rate 143.92 --rotate normal --primary \
         --output DP-4 --mode 1920x1080 --pos 0x0        --rate 143.98 --rotate normal \
-        --output DP-2 --mode 1920x1080 --pos 1920x0     --rate 143.85 --rotate normal \
+        --output DP-0 --mode 1920x1080 --pos 1920x0     --rate 143.85 --rotate normal \
         --output DP-1 --off \
         --output HDMI-0 --off \
         --output DP-3 --off \
