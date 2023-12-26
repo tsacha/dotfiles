519d9c09cf4e560a14cd717350cd960981d47970
Sacha Trémoureux <sacha@tremoureux.fr>
2023-12-26 15:33:20 +0100

    More nvm wtf

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -10,17 +10,6 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-vim.g.coq_settings = {
-  auto_start = 'shut-up',
-  keymap = {
-    pre_select = true
-  },
-  clients = {
-    snippets = {
-      warn = {}
-    }
-  }
-}
 
 require("lazy").setup({
   "tsacha/bepo.nvim",
@@ -37,6 +26,7 @@ require("lazy").setup({
       end,
   },
   "lewis6991/gitsigns.nvim",
+  "rcarriga/nvim-notify",
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { 'nvim-lua/plenary.nvim' }
@@ -53,8 +43,10 @@ require("lazy").setup({
   "ggandor/leap.nvim",
   "williamboman/mason.nvim",
   "williamboman/mason-lspconfig.nvim",
+  "mfussenegger/nvim-dap",
+  "jay-babu/mason-nvim-dap.nvim",
+  "rcarriga/nvim-dap-ui",
   "neovim/nvim-lspconfig",
-  {"ms-jpq/coq_nvim", branch="coq"},
   "nmac427/guess-indent.nvim",
   "nvim-treesitter/nvim-treesitter",
   {
@@ -66,9 +58,6 @@ require("lazy").setup({
       "nvim-tree/nvim-web-devicons", -- optional dependency
     },
     opts = {},
-  },
-  {
-    "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap"}
   },
   {
     "ray-x/go.nvim",
@@ -83,7 +72,16 @@ require("lazy").setup({
     event = {"CmdlineEnter"},
     ft = {"go", 'gomod'},
     build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-  }
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    opts = {} -- this is equalent to setup({}) function
+  },
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/nvim-cmp",
+  "dcampos/nvim-snippy",
+  "dcampos/cmp-snippy",
 })
 
 -- Base settings
@@ -159,16 +157,117 @@ require('gitsigns').setup()
 -- Guess-indent
 require('guess-indent').setup()
 
+-- Autocompletion
+local cmp = require('cmp')
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      require('snippy').expand_snippet(args.body) -- For `snippy` users.
+    end
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'snippy' }
+  }
+}
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 --- LSP
 require("mason").setup()
 require("mason-lspconfig").setup {
     ensure_installed = { "gopls", "terraformls", "ansiblels" }
 }
 
-
 local lsp = require("lspconfig")
-local coq = require "coq"
-lsp.gopls.setup(coq.lsp_ensure_capabilities({}))
+lsp.gopls.setup({capabilities = capabilities})
+
+-- Debuggers
+require("mason-nvim-dap").setup({
+    ensure_installed = { "delve" }
+})
+
+require("dapui").setup({
+  icons = { expanded = "▾", collapsed = "▸" },
+  mappings = {
+    open = "o",
+    remove = "d",
+    edit = "e",
+    repl = "r",
+    toggle = "t",
+  },
+  expand_lines = vim.fn.has("nvim-0.7"),
+  layouts = {
+    {
+      elements = {
+        "scopes",
+      },
+      size = 0.3,
+      position = "right"
+    },
+    {
+      elements = {
+        "repl",
+        "breakpoints"
+      },
+      size = 0.3,
+      position = "bottom",
+    },
+  },
+  floating = {
+    max_height = nil,
+    max_width = nil,
+    border = "single",
+    mappings = {
+      close = { "q", "<Esc>" },
+    },
+  },
+  windows = { indent = 1 },
+  render = {
+    max_type_length = nil,
+  },
+})
+
+local dap_ok, dap = pcall(require, "dap")
+local dap_ui_ok, ui = pcall(require, "dapui")
+
+if not (dap_ok and dap_ui_ok) then
+  require("notify")("nvim-dap or dap-ui not installed!", "warning") -- nvim-notify is a separate plugin, I recommend it too!
+  return
+end
+
+vim.fn.sign_define('DapBreakpoint', { text = '🐞' })
+
+-- Start debugging session
+vim.keymap.set("n", "<leader>ds", function()
+  dap.continue()
+  ui.toggle({})
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false) -- Spaces buffers evenly
+end)
+
+-- Set breakpoints, get variable values, step into/out of functions, etc.
+vim.keymap.set("n", "<leader>dl", require("dap.ui.widgets").hover)
+vim.keymap.set("n", "<leader>dc", dap.continue)
+vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
+vim.keymap.set("n", "<leader>dn", dap.step_over)
+vim.keymap.set("n", "<leader>di", dap.step_into)
+vim.keymap.set("n", "<leader>do", dap.step_out)
+vim.keymap.set("n", "<leader>dC", function()
+  dap.clear_breakpoints()
+  require("notify")("Breakpoints cleared", "warn")
+end)
+
+-- Close debugger and clear breakpoints
+vim.keymap.set("n", "<leader>de", function()
+  dap.clear_breakpoints()
+  ui.toggle({})
+  dap.terminate()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false)
+  require("notify")("Debugger session ended", "warn")
+end)
 
 -- Terraform
 require'lspconfig'.terraformls.setup{}
@@ -191,6 +290,33 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
   group = format_sync_grp,
 })
+
+local dap_ok, dap = pcall(require, "dap")
+if not (dap_ok) then
+  print("nvim-dap not installed!")
+  return
+end
+
+require('dap').set_log_level('INFO') -- Helps when configuring DAP, see logs with :DapShowLog
+
+dap.configurations = {
+    go = {
+      {
+        type = "go", -- Which adapter to use
+        name = "Debug", -- Human readable name
+        request = "launch", -- Whether to "launch" or "attach" to program
+        program = "${file}", -- The buffer you are focused on when running nvim-dap
+      },
+    }
+}
+dap.adapters.go = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    command = vim.fn.stdpath("data") .. '/mason/bin/dlv',
+    args = { "dap", "-l", "127.0.0.1:${port}" },
+  },
+}
 
 -- Treesitter
 require'nvim-treesitter.configs'.setup {
