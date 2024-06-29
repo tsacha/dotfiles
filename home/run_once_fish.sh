32e957ad5931d2f83f54d8331ac541ea57093bba
Sacha Trémoureux <sacha@tremoureux.fr>
2024-06-30 00:55:00 +0200

    du changement (prompt)

--- home/run_once_fish.sh
+++ home/run_once_fish.sh
@@ -2,3 +2,17 @@
 fisher install jethrokuan/z
 fisher install jhillyerd/plugin-git
 fisher install IlanCosman/tide@v6
+
+tide configure \
+    --auto \
+    --style=Lean \
+    --prompt_colors='True color' \
+    --show_time=No \
+    --lean_prompt_height='One line' \
+    --prompt_spacing=Compact \
+    --icons='Few icons' \
+    --transient=No
+
+if contains aws $tide_right_prompt_items
+    set -Ue tide_right_prompt_items[(contains -i aws $tide_right_prompt_items)]
+end
