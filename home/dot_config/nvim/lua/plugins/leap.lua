56b2c5b7d76d13232857c920f75a3123e51fd958
Sacha Trémoureux <sacha@tremoureux.fr>
2024-10-30 18:48:32 +0100

    trc

--- home/dot_config/nvim/lua/plugins/leap.lua
+++ home/dot_config/nvim/lua/plugins/leap.lua
@@ -1,9 +1,22 @@
 return {
-    { -- Adds git related signs to the gutter, as well as utilities for managing changes
-        "ggandor/leap.nvim",
-        config = function()
-            require("leap").add_default_mappings()
-        end,
-    },
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {
+    modes = {
+      search = {
+        enabled = true
+      }
+    }
+
+  },
+  -- stylua: ignore
+  keys = {
+    { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+    { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+    { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+    { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
