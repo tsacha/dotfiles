e3ad52cc15c5e1726a8979b2617f20c4dbf75135
Sacha Trémoureux <sacha@tremoureux.fr>
2025-08-19 00:01:29 +0200

    graou

--- home/dot_config/nvim/lua/plugins/conform.lua
+++ home/dot_config/nvim/lua/plugins/conform.lua
@@ -3,23 +3,13 @@ return {
     "stevearc/conform.nvim",
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
-    keys = {
-      {
-        "<leader>f",
-        function()
-          require("conform").format({ async = true, lsp_format = "fallback" })
-        end,
-        mode = "",
-        desc = "[F]ormat buffer",
-      },
-    },
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = false, cpp = false }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = "never"
@@ -34,11 +24,6 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         yaml = { "yq" },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },
--- home/dot_config/nvim/lua/plugins/mason.lua
+++ home/dot_config/nvim/lua/plugins/mason.lua
@@ -10,6 +10,7 @@ return {
             require("mason").setup()
             require('mason-lspconfig').setup({
                 ensure_installed = {
+                    "clangd",
                     "basedpyright",
                     "gopls",
                     "helm_ls",
