1a21f18923039a237151db511eb07a0649946b83
Sacha Trémoureux <sacha@tremoureux.fr>
2025-11-02 12:06:04 +0100

    lol

--- home/dot_config/nvim/lua/plugins/copilot.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"github/copilot.vim",
-	lazy = false,
-	keys = {
-		{ "<leader>cp", "<cmd>Copilot enable<CR>", desc = "Enable Copilot" },
-		{ "<leader>ck", "<cmd>Copilot disable<CR>", desc = "Disable Copilot" },
-	},
-}
