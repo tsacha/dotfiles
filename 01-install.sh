4a3ec588e598a7ae3020b0fe01609b52985470c6
Sacha Trémoureux <sacha@tremoureux.fr>
2025-11-15 22:48:53 +0100

    reinstall

--- 01-install.sh
+++ 01-install.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-systemd-cryptenroll /dev/nvme1n1p2 --wipe-slot=tpm2 --tpm2-device=auto --tpm2-pcrs=7
 
+# Ça change tout le temps, juste vérifier.
+# archinstall fait du bon taf généralement
 # HOOKS=(base systemd autodetect microcode modconf kms keyboard sd-vconsole block sd-encrypt filesystems fsck
 
+systemd-cryptenroll /dev/nvme1n1p2 --wipe-slot=tpm2 --tpm2-device=auto --tpm2-pcrs=7
 echo "root UUID=$(blkid /dev/nvme1n1p2  -s UUID -o value) none tp2-device=auto" > /etc/crypttab.initramfs
 
-systemctl enable --now NetworkManager
-systemctl enable --now pcscd.service
-systemctl enable --now ly.service
+systemctl enable --now pcscd.service # Yubikey
 
-groupadd uinput
-usermod -aG input sacha
-usermod -aG uinput sacha
-
-echo 'KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"' > /etc/udev/rules.d/99-input.rules
-
+cp ./home/dot_layouts/ergol.xkb_symbols /usr/share/X11/xkb/symbols/ergol_anglemod
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
