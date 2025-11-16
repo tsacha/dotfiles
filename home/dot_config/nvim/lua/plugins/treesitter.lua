96c15bbc129404f02556da605979ff023ab166c4
Sacha Trémoureux <sacha@tremoureux.fr>
2025-11-16 20:52:41 +0100

    gleamsamere

--- home/dot_config/nvim/lua/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,19 +3,31 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		main = "nvim-treesitter.configs",
-		opts = {
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = false },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<A-o>",
-					node_incremental = "<A-o>",
-					scope_incremental = "<A-O>",
-					node_decremental = "<A-i>",
+		opts = function(_plugin, opts)
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			parser_config.gleam = {
+				install_info = {
+					url = "https://github.com/gleam-lang/tree-sitter-gleam",
+					revision = "main",
+					files = { "src/parser.c", "src/scanner.c" },
 				},
-			},
-		},
+				filetype = "gleam",
+			}
+
+			return vim.tbl_deep_extend("force", opts, {
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = false },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<A-o>",
+						node_incremental = "<A-o>",
+						scope_incremental = "<A-O>",
+						node_decremental = "<A-i>",
+					},
+				},
+			})
+		end,
 	},
 }
