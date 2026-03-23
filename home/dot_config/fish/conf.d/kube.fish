44b8a3e56cda7b090b8f99a55318545aa66ff212
Sacha Trémoureux <sacha@tremoureux.fr>
2026-03-23 21:47:34 +0100

    kube

--- home/dot_config/fish/conf.d/kube.fish
+++ home/dot_config/fish/conf.d/kube.fish
@@ -1,4 +1,22 @@
 set -gx K9S_CONFIG_DIR $HOME/.config/k9s
 
-alias k="kubectl"
-alias ku="kubie"
+abbr --add k kubectl
+abbr --add ku kubie
+
+set -l k_shortcuts \
+    gp="get pods" \
+    gd="get deployments" \
+    gs="get services" \
+    gn="get nodes" \
+    gcr="get cronjobs" \
+    gj="get jobs" \
+    gns="get namespaces" \
+    lo="logs -f"
+
+for pair in $k_shortcuts
+    set -l name (string split -m 1 "=" $pair)[1]
+    set -l value (string split -m 1 "=" $pair)[2]
+    abbr --add $name --command kubectl $value
+end
+abbr --add n --command kubectl -- --namespace
+abbr --add w --command kubectl -- --watch
